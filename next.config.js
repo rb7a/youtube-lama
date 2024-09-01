2cb98b73a5454105353449ca75ef7fdd4b0cd736
rb7a <130118715+rb7a@users.noreply.github.com>
2024-09-01 14:57:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "images.pexels.com"],
+    domains: ["localhost", "images.pexels.com", "next-agency-demo-site.netlify.app],
     loader: "imgix",
     path: "",
   },
