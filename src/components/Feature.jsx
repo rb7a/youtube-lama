8b9827e2932b62c68f530a55ba366e96593e82c4
rb7a <130118715+rb7a@users.noreply.github.com>
2024-09-02 11:58:13 +0800

    Update Feature.jsx

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import App from ".././img/app.png";
+import App from "././img/app.png";
 import AnimatedShapes from "./AnimatedShapes";
 
 const Container = styled.div`
